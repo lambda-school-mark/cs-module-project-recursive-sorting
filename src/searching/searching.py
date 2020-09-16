116a2ea46bfc5990fdac0eb17d10ce32caab2d18
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-09-16 11:22:24 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,35 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
+    if end >= start:
+
+        mid = (end + start) // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == target:
+            return mid
+
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > target:
+            return binary_search(arr, target, start, mid - 1)
+
+        # Else the element can only be present in right subarray
+        else:
+            return binary_search(arr, target, mid + 1, end)
+    else:
+        # Element is not present in the array
+        return -1
 
 
 # STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
+# This version of binary search should correctly find
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
-# You can implement this function either recursively 
+# You can implement this function either recursively
 # or iteratively
+
+
 def agnostic_binary_search(arr, target):
     # Your code here
-
+    pass
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,63 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    merged_arr = []
 
-    # Your code here
+    # pointers for array a and b
+    a = 0
+    b = 0
 
+    # while the array's have elements
+    while a < len(arrA) and b < len(arrB):
+        if arrA[a] < arrB[b]:
+            merged_arr.append(arrA[a])
+            a += 1
+
+        else:
+            merged_arr.append(arrB[b])
+            b += 1
+    # elements merged in from one of the arrays, but not the other
+
+    # check each array for leftovers
+    if a < len(arrA):
+        # push leftover elements to the merged array
+        merged_arr.extend(arrA[a:])
+
+    if b < len(arrB):
+        # push leftover elements to the merged array
+        merged_arr.extend(arrB[b:])
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
-    # Your code here
-
-
+    # if less than 2 elements, there isn nothing to merge or sort
+    if len(arr) < 2:
+        return arr
+    # if it's unsorted:
+    else:
+        # determine the midpoint,
+        # and set our left and right arrays using that point
+        midpoint = len(arr) // 2
+        left = arr[0:midpoint]
+        right = arr[midpoint:]
+        # our finished array will be the result of a merge
+        # between the recursive calls of merge sort which will return arrays when sorted
+        arr = merge(merge_sort(left), merge_sort(right))
     return arr
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
+
+
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
